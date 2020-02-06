6bfbc1d4b24bb7d3941b3fdbd64ae67849ba9905
Alec <apetzak1@gmail.com>
2020-02-05 21:00:10 -0500

    CursorSwitcher and icons

---
+++ Project Harvest/Assets/Scripts/CursorSwitcher.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorSwitcher : MonoBehaviour
+{
+    public static CursorSwitcher Instance { get; } = new CursorSwitcher();
+
+    public List<Texture2D> textures;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    void Start()
+    {
+        Instance.textures = textures;
+    }
+
+    public void Set(int i)
+    {
+        if (Instance.textures.Count <= i)
+            return;
+
+        Cursor.SetCursor(Instance.textures[i], hotSpot, cursorMode);
+    }
+}
